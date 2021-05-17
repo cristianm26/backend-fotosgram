9744ca0b9c0def0412cfe13aff234de47c0193df
hugo@DESKTOP-J7IENLF <cristian260893@gmail.com>
2021-05-17 17:26:49 -0500

    Uploads

--- index.ts
+++ index.ts
@@ -2,16 +2,21 @@ import Server from "./classes/server";
 import mongoose from 'mongoose';
 import userRoutes from './routes/usuarioroutes';
 import express from 'express';
-import morgan from 'morgan'
+import morgan from 'morgan';
+import fileUpload from 'express-fileupload'
+import postRoutes from "./routes/postroutes";
 const server = new Server();
 
 // Lectura y parseo del body
+
 server.app.use(express.json());
+//FileUpload
+server.app.use(fileUpload({useTempFiles: true}));
 //Utilizar morgan
 server.app.use(morgan('dev'));
 //Rutas de mi aplicacion
 server.app.use('/user' ,userRoutes )
-
+server.app.use('/posts', postRoutes)
 //Conectar la base de Datos de MongoDB
 mongoose.connect('mongodb://localhost:27017/fotosgram', {
     useNewUrlParser: true,
